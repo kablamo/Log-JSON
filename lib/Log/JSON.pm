bcdf6880e4ef5ea359e2b3f8e519cbe063be8518
Eric Johnson <eric.git@iijo.org>
2012-04-28 22:28:12 +0100

    Updates to POD.

--- lib/Log/JSON.pm
+++ lib/Log/JSON.pm
@@ -1,4 +1,3 @@
-# ABSTRACT: Log data to a file as JSON
 package Log::JSON;
 use Moose;
 use MooseX::Types::Path::Class;
@@ -8,6 +7,69 @@ use DateTime;
 use English;
 use JSON;
 use Path::Class::File;
+
+# VERSION
+
+=attr date
+
+Adds an __date field to your json.  The '__' part ensures the date is the first
+information logged to each line when the keys are sorted.
+
+=cut
+has 'date' => ( is => 'ro', isa => 'Bool', default => 1 );
+
+=attr file
+
+The name of the file to log data to
+
+=cut
+has 'file' => (
+    is      => 'ro',
+    isa     => 'Path::Class::File',
+    required => 1,
+    coerce  => 1,
+);
+
+=attr remove_newlines
+
+This boolean is set to true by default.  It means your jason data structures
+will be logged entirely on one line.
+
+=cut
+has 'remove_newlines' => ( is => 'ro', isa => 'Bool', default => 1 );
+
+sub BUILD {
+    my $self = shift;
+    $self->file->dir->mkpath;
+    $self->file->touch;
+}
+
+=method log(%hash)
+
+Appends %hash to a file as JSON.  The keys are sorted when the hash is converted to JSON.
+
+=cut
+sub log {
+    my $self = shift;
+    my %data = @_;
+
+    $data{__date} = DateTime->now . ""
+        if $self->date;
+
+    #                     sort keys
+    my $json = JSON->new->canonical->encode( \%data );
+
+    if ( $self->remove_newlines ) {
+        $json =~ s/\n//g;
+        $json =~ s/\r//g;
+    }
+
+    my $fh = $self->file->open('>>');
+    print $fh $json . "\n";
+    $fh->close;
+}
+
+# ABSTRACT: Log data to a file as JSON
 
 =head1 SYNOPSIS
 
@@ -33,89 +95,18 @@ information is labeled.
 Using JSON also means log files are easy to parse and the data structures can
 be easily revived.
 
-=head1 ATTRIBUTES
-
-=head2 file
-
-The name of the file to log data to
-
-=head2 date
-
-Adds an __date field to your json.  The '__' part ensures the date is the first
-information logged to each line when the keys are sorted.
-
-=head2 remove_newlines
-
-This boolean is set to true by default.  It means your jason data structures
-will be logged entirely on one line.
-
-=cut
-
-
-has 'remove_newlines' => ( is => 'ro', isa => 'Bool', default => 1 );
-has 'date'            => ( is => 'ro', isa => 'Bool', default => 1 );
-
-has 'file' => (
-    is      => 'ro',
-    isa     => 'Path::Class::File',
-    required => 1,
-    coerce  => 1,
-);
-
-sub BUILD {
-    my $self = shift;
-    $self->file->dir->mkpath;
-    $self->file->touch;
-}
-
-=head1 METHODS
-
-=cut
-
-=head2 new(%attributes)
-
-Returns a Log::JSON object.
-
-=cut
-
-=head2 log(%hash)
-
-Appends %hash to a file as JSON.  The keys are sorted when the hash is converted to JSON.
-
-=cut
-
-sub log {
-    my $self = shift;
-    my %data = @_;
-
-    $data{__date} = DateTime->now . ""
-        if $self->date;
-
-    #                     sort keys
-    my $json = JSON->new->canonical->encode( \%data );
-
-    if ( $self->remove_newlines ) {
-        $json =~ s/\n//g;
-        $json =~ s/\r//g;
-    }
-
-    my $fh = $self->file->open('>>');
-    print $fh $json . "\n";
-    $fh->close;
-}
-
 =head1 BUGS
 
 This should probably should have been a Log::Dispatch plugin.
+
+=head1 SEE ALSO
+
+L<Log::Message::Structured>, L<Log::Structured>, L<Log::Sprintf>
 
 =head1 THANKS
 
 Thanks to Foxtons Ltd for providing the opportunity to write and release the
 original version of this module.
-
-=head1 SEE ALSO
-
-L<Log::Message::Structured>, L<Log::Structured>, L<Log::Sprintf>
 
 =cut
 
